9a8db4c0e22b2d53c032d788a0a97110581e9ce1
theumang100 <theumangpatelu2p@gmail.com>
2023-03-30 11:23:41 -0700

    about updated

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,13 +6,15 @@ function About() {
     return (
         <>
             <Header />
-            <Description title="About." subtitle="I'm Umang Patel, computer engineer." />
+            <Description title="About." subtitle="I'm Umang Patel, Software Engineer." />
             <div className="about_me_info">
                 <div className="container">
                     <div className="row no-gutters">
                         <div className="col-md-12">
 
-                            <p>Hey, I'm umang!. I'm a <b>computer</b> aka <b>software engineer</b> working in <b>United States of America</b>. Welcome to my spot on the web for projects I've created, tutorials I've written, reflection of my mind.</p>
+                            <p>Hey, I'm umang!. I'm a <b>computer</b> aka <b>software engineer</b> in <b>United States of America</b>.</p>
+                            <p>Currently I am in final semester of Master's in Computer Science at California State University - Channel Islands, Camarillo, California.</p>
+                            <p>I like to develop softwares and solving errors. In my free time I like to do programming, listen music or play volley ball with friends if anyone is available:)</p>
                             <p>This project is made with ReactJS technology and bootstrap framework. Hosting platform is aws.</p>
 
                         </div>
